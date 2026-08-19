93acb23d46c9ec119e43ddabb2c27352a330aad1
luongphatlp <luongphat1123@gmail.com>
2026-08-19 09:40:28 +0700

    khoi tao

---
+++ src/quan_li_ban_hang/Quan_li_ban_hang.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quan_li_ban_hang;
+
+/**
+ *
+ * @author Latitude E7470
+ */
+public class Quan_li_ban_hang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
